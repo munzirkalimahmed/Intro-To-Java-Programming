9c236577512926a18e69d4dc3787efe357938f43
Munzir Kalim Ahmed <72119116+munzirkalimahmed@users.noreply.github.com>
2022-02-17 12:21:08 +0500

    Add files via upload

---
+++ Java/Intro01.java
@@ -0,0 +1,8 @@
+public class Intro01 {
+   public static void main(String args[]) {
+   
+   	System.out.println("Hello world!");
+   	System.out.println(args[0]);
+   	System.out.println(args[1]);
+   }
+}
